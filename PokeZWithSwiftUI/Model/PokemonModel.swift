2cab2904906fd66741f22ea76b78c50477ba105f
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-01 13:39:07 +0900

    モデル

---
+++ PokeZWithSwiftUI/Model/PokemonModel.swift
@@ -0,0 +1,37 @@
+//
+//  PokemonModel.swift
+//  PokeZWithSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/01.
+//
+
+import Foundation
+
+struct PokemonModel: Codable {
+    
+}
+
+struct Pokemon: Decodable {
+    let name: String
+    let id: Int
+    let sprites: Images
+    let types: [Types]
+}
+
+struct Images: Codable {
+    let frontImage: String
+    let shinyImage: String
+
+    enum CodingKeys: String, CodingKey {
+        case frontImage = "front_default"
+        case shinyImage = "front_shiny"
+    }
+}
+
+struct Types: Codable {
+    let type: Type
+}
+
+struct Type: Codable {
+    let name: String
+}
