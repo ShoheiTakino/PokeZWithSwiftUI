ca7e3e767bc2a7b873cb3112906237bb6b221dc6
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-01 13:39:02 +0900

    メインビュー

--- PokeZWithSwiftUI/ContentView.swift
+++ PokeZWithSwiftUI/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var pokeData = PokeRequest()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Task {
+                await pokeData.fetchPokemonData()
+            }
         }
         .padding()
     }
