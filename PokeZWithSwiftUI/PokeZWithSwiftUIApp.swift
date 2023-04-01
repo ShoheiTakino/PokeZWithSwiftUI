ff203fb9dd704b03665cb7798e7b6156b7c96c32
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-01 13:16:12 +0900

    Initial Commit

---
+++ PokeZWithSwiftUI/PokeZWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeZWithSwiftUIApp.swift
+//  PokeZWithSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct PokeZWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
