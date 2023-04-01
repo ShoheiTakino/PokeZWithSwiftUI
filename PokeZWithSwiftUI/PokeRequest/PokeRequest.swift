c894a17637e5c2de40c217cd23eef654cc1af6c2
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-01 14:51:26 +0900

    リクスト

--- PokeZWithSwiftUI/PokeRequest/PokeRequest.swift
+++ PokeZWithSwiftUI/PokeRequest/PokeRequest.swift
@@ -7,18 +7,43 @@
 
 import Foundation
 
+enum ApiRequestError: Error {
+    case ConnectionError
+    case ServerError
+}
+
 final class PokeRequest: ObservableObject {
     
-    func fetchPokemonData() async {
-        guard let url = URL(string: "ttps://pokeapi.co/api/v2/ability/1/") else { return }
-        print(#function, url)
+    @Published var pokemonList: [Pokemon] = []
+    
+    func fetchPokemonData() async -> [Pokemon]? {
+        print(#function, 1)
+        let id = 200
+        var urlList: [URL] = []
+        for i in 1..<id {
+            guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(i)/") else { return nil }
+            urlList.append(url)
+        }
+        
+        print(#function, urlList)
         do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let decoder = JSONDecoder()
-            let json = try decoder.decode(Pokemon.self, from: data)
-            print(#function, json)
+            for i in 0..<urlList.count {
+                let (data, _) = try await URLSession.shared.data(from: urlList[i])
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(Pokemon.self, from: data)
+                print(#function, json)
+                pokemonList.append(json)
+            }
+            return pokemonList
+//            for i in 1..<id {
+//                let (data, _) = try await URLSession.shared.data(from: urlList[i])
+//                let decoder = JSONDecoder()
+//                let json = try decoder.decode(Pokemon.self, from: data)
+//                print(#function, json)
+//            }
         } catch {
             print(#function, "エラー")
         }
+        return nil
     }
 }
